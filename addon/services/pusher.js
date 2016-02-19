16456c4f423a499ce24c09174689a5ffaf675c99
Michał Olejniczak <michal.olejniczak@netguru.pl>
2016-02-19 15:08:07 +0100

    add main service which handles integration

---
+++ addon/services/pusher.js
@@ -0,0 +1,63 @@
+import Ember from 'ember';
+import Checker from 'ember-pusher-guru/mixins/checker';
+import { fetchEvents } from 'ember-pusher-guru/utils/extract-events';
+
+const { getOwner, computed } = Ember;
+
+export default Ember.Service.extend(Ember.Evented, Checker, {
+  pusher: null,
+  pusherKey: null,
+  channels: [],
+
+  init() {
+    this._super.apply(this, arguments);
+    this.set('pusherKey', getOwner(this).resolveRegistration('config:environment').pusherKey);
+    this.setup();
+  },
+
+  setup() {
+    this.checkEnv();
+    this.pusher = new window.Pusher(this.get('pusherKey'));
+    const handleEvent = Ember.run.bind(this, this._handleEvent);
+    this.on('newEvent', handleEvent);
+    this._setSubscriptions();
+    this._attachEventsToChannels();
+  },
+
+  _handleEvent(action, data) {
+    this[action](data);
+  },
+
+  _setSubscriptions() {
+    this.get('channelsData').forEach((singleChannel) => {
+      const channelName = Object.keys(singleChannel)[0];
+      const channel = this._addChannel(channelName);
+      this.get('channels').push(channel);
+    });
+  },
+
+  _addChannel(name) {
+    return this.get('pusher').subscribe(name);
+  },
+
+  _attachEventsToChannels() {
+    this.get('channels').forEach((channel) => {
+      const events = fetchEvents(this.get('channelsData'), channel.name);
+      events.forEach((event) => {
+        this._setEvent(channel, event);
+      });
+    });
+  },
+
+  _setEvent(channel, event) {
+    channel.bind(event, (data) => {
+      this.trigger('newEvent', event, data);
+    });
+  },
+
+  willDestroy() {
+    if (this.pusher) {
+      this.pusher.disconnect();
+    }
+  }
+});
