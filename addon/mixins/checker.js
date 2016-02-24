b0750b5e4e10f9dc1ca2e7fce0157fe236ec4cae
Michał Olejniczak <michal.olejniczak@netguru.pl>
2016-02-24 12:07:24 +0100

    extend checker for correctly data structure

--- addon/mixins/checker.js
+++ addon/mixins/checker.js
@@ -1,9 +1,39 @@
 import Ember from 'ember';
+const { assert } = Ember;
 
 export default Ember.Mixin.create({
   checkEnv() {
-    Ember.assert("You need to set pusherKey in environment", !!this.get('pusherKey'));
-    Ember.assert("You need to include the pusher libraries", !!window.Pusher);
-    Ember.assert("You need to define channelsData property", !!this.channelsData);
+    assert("You need to set pusherKey in environment", !!this.get('pusherKey'));
+    assert("You need to include the pusher libraries", !!window.Pusher);
+    assert("You need to define channelsData property", !!this.channelsData);
+    assert("channelsData property has a wrong structure", !this._checkDataStructure(this.get('channelsData')));
   },
+
+  _checkDataStructure(data) {
+    return (Array.isArray(data) && this._findWrongData(data)) ? true : false
+  },
+
+  _isArray(array) {
+    debugger;
+    return Array.isArray(array);
+  },
+
+  _findWrongData(array) {
+    const wrong = array.find((channel) => {
+      return this._nonHashElements(channel) || this._detectEmptyHash(channel) || this._checkMissingEvents(channel);
+    });
+    return wrong === undefined ? true : false
+  },
+
+  _nonHashElements(channel) {
+    return typeof channel !== 'object';
+  },
+
+  _detectEmptyHash(channel) {
+    return Object.keys(channel).length === 0;
+  },
+
+  _checkMissingEvents(channel) {
+    return channel[Object.keys(channel)[0]].length === 0;
+  }
 });
