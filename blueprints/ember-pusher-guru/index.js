d67bd5a946ccefcd46870efddc06972786a6eaf7
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-03-03 11:24:34 +0100

    fix afterInstall hook

--- blueprints/ember-pusher-guru/index.js
+++ blueprints/ember-pusher-guru/index.js
@@ -2,7 +2,10 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    this.addBowerPackageToProject('pusher-test-stub', '2.0.0');
-    return this.addBowerPackageToProject('pusher', '3.0.0');
+    var blueprint = this;
+
+    return blueprint.addBowerPackageToProject('pusher', '3.0.0').then(function() {
+      return blueprint.addBowerPackageToProject('pusher-test-stub', '2.0.0');
+    });
   }
 };
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 module.exports = {
   name: 'ember-pusher-guru',
 
-  included(app) {
+  included: function(app) {
     this._super.included(app);
 
     app.import(app.bowerDirectory + '/pusher/dist/pusher.js');
