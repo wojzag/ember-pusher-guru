942365999e7cac29c98ae56d98c8ce66d961f342
Kuba Niechciał <jakub.niechcial@netguru.co>
2016-05-27 11:57:43 +0200

    Remove the test that is basically useless

--- tests/acceptance/pusher-test.js
+++ tests/acceptance/pusher-test.js
@@ -20,20 +20,3 @@ test('handles event that is listening for', function(assert) {
     assert.equal(find('#message').text(), 'stone');
   });
 });
-
-test('does not handle event if is not listening for', function(assert) {
-  visit('/');
-
-  andThen(function() {
-    try {
-      triggerEvent('running', 'splash');
-    } catch(err) {
-      if (err.message !== 'Expecting a function in instanceof check, but got undefined') {
-        throw err;
-      }
-    }
-    Ember.run.next(() => {
-      assert.equal(find('#message').text(), '');
-    });
-  });
-});
