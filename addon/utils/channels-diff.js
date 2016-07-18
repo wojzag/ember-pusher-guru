ce32656b57cb75ba7394c3b650057fc111154df9
jbandura <jacek.bandura@netguru.pl>
2016-07-18 16:47:59 +0200

    rename function

--- addon/utils/channels-diff.js
+++ addon/utils/channels-diff.js
@@ -1,4 +1,4 @@
-function channelIncluded(channelData, channelsList) {
+function channelNotIncluded(channelData, channelsList) {
   const [channelName] = Object.keys(channelData);
   return channelsList.indexOf(channelName) < 0;
 }
@@ -13,9 +13,9 @@ export function channelsDiff(oldChannelsData, newChannelsData) {
   const oldChannels = oldChannelsData.map(channel => Object.keys(channel)[0]);
   const newChannels = newChannelsData.map(channel => Object.keys(channel)[0]);
   const channelsToUnsubscribe = oldChannelsData
-    .filter((channel) => channelIncluded(channel, newChannels));
+    .filter((channel) => channelNotIncluded(channel, newChannels));
   const channelsToSubscribe = newChannelsData
-    .filter((channel) => channelIncluded(channel, oldChannels));
+    .filter((channel) => channelNotIncluded(channel, oldChannels));
 
   return { channelsToUnsubscribe, channelsToSubscribe };
 }
